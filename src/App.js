7e2d6cf29b140b1e7898e3f329594ff9769b800b
yooseungkim <91931704+yooseungkim@users.noreply.github.com>
2022-02-09 11:53:14 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,18 +4,66 @@ import DisplayUsers from "./js/DisplayUsers";
 import NavigationBar from "./js/NavigationBar";
 
 function App() {
+  let initialPreferences = {
+    displayIcon: true,
+    displayMoney: true,
+  };
+  let [preferences, setPreferences] = React.useState(initialPreferences);
+
+  localStorage.getItem("preferences") === null
+    ? localStorage.setItem("preferences", JSON.stringify(initialPreferences))
+    : (preferences = JSON.parse(localStorage.getItem("preferences")));
+
   const [users, setUsers] = React.useState([
-    { id: 1, name: "yooseung", type: "Manager" },
-    { id: 2, name: "sungjun", type: "Depositors" },
-    { id: 3, name: "jangwon", type: "Depositors" },
-    { id: 4, name: "haeun", type: "Recipients" },
-    { id: 5, name: "prof.Hwang", type: "Recipients" },
+    {
+      id: 1,
+      name: "yooseung",
+      type: "Manager",
+      account: 111111,
+      spent: 200000,
+      events: ["a"],
+    },
+    {
+      id: 2,
+      name: "sungjun",
+      type: "Depositors",
+      account: 111112,
+      spent: -100000,
+      events: ["b"],
+    },
+    {
+      id: 3,
+      name: "jangwon",
+      type: "Depositors",
+      account: 111113,
+      spent: -300000,
+      events: ["c"],
+    },
+    {
+      id: 4,
+      name: "haeun",
+      type: "Recipients",
+      account: 111114,
+      spent: 100000,
+      events: ["c"],
+    },
+    {
+      id: 5,
+      name: "prof.Hwang",
+      type: "Recipients",
+      account: 111115,
+      spent: 100000,
+      events: ["a"],
+    },
   ]);
 
   return (
     <div className="App">
-      <DisplayUsers users={users} />
-      <NavigationBar />
+      <DisplayUsers users={users} preferences={preferences} />
+      <NavigationBar
+        preferences={preferences}
+        setPreferences={setPreferences}
+      />
     </div>
   );
 }
--- src/js/NavigationBar.js
+++ src/js/NavigationBar.js
@@ -1,11 +1,45 @@
 import React from "react";
 import gearSrc from "../img/gearshape.png";
 import bracketSrc from "../img/leftAngleBracket.jpeg";
+import DisplayUsers from "./DisplayUsers";
 
-function NavigationBar() {
+function NavigationBar({ preferences, setPreferences }) {
+  const [tabActive, setTabActive] = React.useState("none");
+
+  const onClickDisplayIcon = () => {
+    let newPreference = preferences;
+    newPreference.displayIcon = !preferences.displayIcon;
+    setPreferences(newPreference);
+    localStorage.setItem("preferences", JSON.stringify(preferences));
+  };
+  const onClickDisplayMoney = () => {
+    let newPreference = preferences;
+    newPreference.displayMoney = !preferences.displayMoney;
+    setPreferences(newPreference);
+    localStorage.setItem("preferences", JSON.stringify(preferences));
+  };
+  function PreferenceTab() {
+    return (
+      <div style={{ display: tabActive }}>
+        <span onClick={onClickDisplayIcon}>
+          Display Icon: {preferences.displayIcon ? "ON" : "OFF"}
+        </span>
+        <br />
+        <span onClick={onClickDisplayMoney}>
+          Display Money: {preferences.displayMoney ? "ON" : "OFF"}
+        </span>
+        <br />
+      </div>
+    );
+  }
+
+  const onClickPreference = () => {
+    setTabActive(tabActive === "block" ? "none" : "block");
+  };
   return (
     <div className="bar">
-      <img className="barIcon" src={gearSrc} />
+      <img className="barIcon" src={gearSrc} onClick={onClickPreference} />
+      <PreferenceTab />
       <br />
       <img className="barIcon" src={bracketSrc} />
       <br />
